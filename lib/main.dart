bae83afa6bf37c1f442ee86b900f0cdfb0b8e9bc
Kunwoo Shin <tensorflow555@gmail.com>
2024-07-22 05:19:44 +0900

    Stateless & Stateful Widget 생성 코드

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainScreen(),
+      home: const MainScreen2(),
     );
   }
 }
@@ -44,11 +44,49 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
+      appBar: AppBar(title: Text('어플리케이션'),),
       body: Text('안녕하세요'),
     );
   }
 }
+
+/**
+ * StatefulWidget을 구현한 MainScreen2
+ */
+class MainScreen2 extends StatefulWidget {
+  const MainScreen2({super.key});
+
+  @override
+  State<MainScreen2> createState() => _MainScreen2State();
+}
+
+// Spring의 PostConstructor 같은 느낌의 initState를 이용해 Body의 문구를 3초 후 바꿈 -> 상태 변경
+class _MainScreen2State extends State<MainScreen2> {
+  String title = '어플리케이션';
+  String msg = '안녕하세요';
+
+  @override
+  void initState() {
+    super.initState();
+    
+    Future.delayed(Duration(seconds: 3), () {
+      setState(() {
+        msg = '안녕하세요2';
+        title = '어플리케이션2';
+      });
+    },);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title),),
+      body: Text(msg),
+    );
+  }
+}
+
 
 
 
