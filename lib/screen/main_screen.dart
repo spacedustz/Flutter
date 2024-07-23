bec025e75b40bdc83df28ca0985db8f3518d18f5
Kunwoo Shin <tensorflow555@gmail.com>
2024-07-24 03:46:59 +0900

    Column-Row-Expanded Widget 코드 추가

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -11,8 +11,31 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('메인화면'),
+      ),
       body: Center(
-        child: Text('메인 화면 입니다.'),
+        // child: Text('메인 화면 입니다.'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('안녕하세요.'),
+            Text('개발자 신건우입니다.'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text('이건'),
+                Text('세로축인 Row Widget')],
+            ),
+            Row(
+              children: [
+                Expanded(flex: 2, child: Text('홍길동')),
+                Expanded(child: Text('홍길동')),
+                Expanded(child: Text('홍길동')),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
