3d03c960983a098950714f533b559b979ea9ec25
Kunwoo Shin <recyclefunction@gmail.com>
2024-07-23 19:57:31 +0900

    Route - Splash Screen & Main Screen 화면 매핑 코드 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ex/screen/main_screen.dart';
+import 'package:flutter_ex/screen/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,65 +13,80 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainScreen2(),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => SplashScreen(),
+        '/main': (context) => MainScreen(),
+      },
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      //   useMaterial3: true,
+      // ),
     );
   }
 }
 
-/**
- * StatelessWidget을 상속한 MainScreen Widjet
- */
-class MainScreen extends StatelessWidget {
-  const MainScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('어플리케이션'),),
-      body: Text('안녕하세요'),
-    );
-  }
-}
 
-/**
- * StatefulWidget을 구현한 MainScreen2
- */
-class MainScreen2 extends StatefulWidget {
-  const MainScreen2({super.key});
 
-  @override
-  State<MainScreen2> createState() => _MainScreen2State();
-}
 
-// Spring의 PostConstructor 같은 느낌의 initState를 이용해 Body의 문구를 3초 후 바꿈 -> 상태 변경
-class _MainScreen2State extends State<MainScreen2> {
-  String title = '어플리케이션';
-  String msg = '안녕하세요';
 
-  @override
-  void initState() {
-    super.initState();
-    
-    Future.delayed(Duration(seconds: 3), () {
-      setState(() {
-        msg = '안녕하세요2';
-        title = '어플리케이션2';
-      });
-    },);
-  }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text(title),),
-      body: Text(msg),
-    );
-  }
-}
+
+
+
+/* -------------------- Stateless & Stateful Widget 예시 코드 -------------------- */
+
+// /**
+//  * StatelessWidget을 상속한 MainScreen Widjet
+//  */
+// class MainScreen extends StatelessWidget {
+//   const MainScreen({super.key});
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('어플리케이션'),),
+//       body: Text('안녕하세요'),
+//     );
+//   }
+// }
+//
+// /**
+//  * StatefulWidget을 구현한 MainScreen2
+//  */
+// class MainScreen2 extends StatefulWidget {
+//   const MainScreen2({super.key});
+//
+//   @override
+//   State<MainScreen2> createState() => _MainScreen2State();
+// }
+//
+// // Spring의 PostConstructor 같은 느낌의 initState를 이용해 Body의 문구를 3초 후 바꿈 -> 상태 변경
+// class _MainScreen2State extends State<MainScreen2> {
+//   String title = '어플리케이션';
+//   String msg = '안녕하세요';
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//
+//     Future.delayed(Duration(seconds: 3), () {
+//       setState(() {
+//         msg = '안녕하세요2';
+//         title = '어플리케이션2';
+//       });
+//     },);
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text(title),),
+//       body: Text(msg),
+//     );
+//   }
+// }
 
 
 
