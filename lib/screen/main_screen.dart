bc0dfdf3a20aeb188f4a75fca40f3da88ce7c073
owen <wavefunctionc@gmail.com>
2024-07-29 18:58:07 +0900

    Navigator 임시 저장

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -8,32 +8,49 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  // 사용자의 입력을 컨트롤러가 받음
+  TextEditingController idController = TextEditingController();
+  String stateMessage = '이 곳에 값이 업데이트 됩니다.';
+  ValueNotifier<int> counter = ValueNotifier<int>(0); // 0으로 초기화
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('메인화면'),
-        ),
-        body: Container( // 버튼의 크기를 늘리기 위한 컨테이너
-          alignment: Alignment.center,
-          // width: 200,
-          // height: 100,
-          margin: EdgeInsets.all(32),
-
-          child: Column(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    print('버튼이 클리 되었습니다.');
-                  },
-                  style: ElevatedButton.styleFrom( // 버튼 스타일
-                    backgroundColor: Colors.white, // 배경색
-                    foregroundColor: Colors.black, // 글자색
-                    elevation: 10 // 그림자
-                  ),
-                  child: Text('버튼 누르기'))
-            ],
+      appBar: AppBar(
+        title: Text('메인화면'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            controller: idController,
+            decoration: InputDecoration(labelText: '아이디를 입력해 주세요.'),
           ),
-        ));
+          // 버튼을 이용해 사용자의 입력값을 컨트롤러에서 가져와 Console에 찍기
+          ElevatedButton(
+            onPressed: () {
+              // State가 변경되면 stateMessage 변수에 사용자의 입력값을 넣음
+              setState(() {
+                stateMessage = idController.text.toString();
+              });
+
+              counter.value = 30;
+            },
+            child: Text('아이디 입력값 가져오기!'),
+          ),
+
+          // 값을 지속적으로 Listen
+          ValueListenableBuilder<int>(
+            valueListenable: counter,
+            builder: (context, value, child) {
+              return Text('Count : $value');
+            },
+          ),
+          Text(
+            '사용자 입력값 = $stateMessage',
+            style: TextStyle(fontSize: 20),
+          ),
+        ],
+      ),
+    );
   }
 }
