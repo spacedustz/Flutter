ceffd3815d7512e09ab1c44e7aa49fb669526cda
Kunwoo Shin <recyclefunction@gmail.com>
2024-08-15 15:49:41 +0900

    노트앱 - UI 라우팅 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ex/note/edit_screen.dart';
 
+import 'note/data/note_info.dart';
 import 'note/main_screen.dart';
 import 'note/splash_screen.dart';
 
@@ -23,8 +25,22 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         // 명함 앱
+        // '/': (context) => SplashScreen(),
+        // '/main': (context) => MainScreen(),
+
+        // 아이디어 앱
         '/': (context) => SplashScreen(),
         '/main': (context) => MainScreen(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/edit') {
+          // 데이터 객체가 없으면 등록 화면, 있으면 수정 화면 라우팅
+          final NoteInfo? data = settings.arguments as NoteInfo?; // null 처리
+          
+          return MaterialPageRoute(builder: (context) {
+            return EditScreen(data: data,);
+          },);
+        }
       },
     );
   }
