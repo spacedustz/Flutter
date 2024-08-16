d0e399aa776d8a838538582a98e0e04b664ce7d8
owen <floralexium@gmail.com>
2024-08-16 23:38:18 +0900

    노트앱 - 상세 화면 만들기 시작

---
+++ lib/note/detail_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'data/note_info.dart';
+
+class DetailScreen extends StatelessWidget {
+  NoteInfo? data;
+
+  DetailScreen({super.key, this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
